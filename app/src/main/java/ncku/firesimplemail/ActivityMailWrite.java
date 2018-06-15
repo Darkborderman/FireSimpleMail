5e12f0e8be9c15bcea1079fae49fadbd7f79c514
Darkborderman <reastw1234@gmail.com>
2018-06-15 15:20:15 +0800

    ==Add mail write layout== Add mail write layout and connect with client

--- app/src/main/java/ncku/firesimplemail/ActivityMailWrite.java
+++ app/src/main/java/ncku/firesimplemail/ActivityMailWrite.java
@@ -1,15 +1,57 @@
 package ncku.firesimplemail;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ActivityMailWrite extends AppCompatActivity{
+
+    Button sendButton;
+    TextView titleTextBox,fromTextBox,toTextBox,contextTextBox;
+    String title,from,to,context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_mail_write);
+
+        sendButton=findViewById(R.id.sendButton);
+        titleTextBox=findViewById(R.id.titleTextBox);
+        fromTextBox=findViewById(R.id.fromTextBox);
+        toTextBox=findViewById(R.id.toTextBox);
+        contextTextBox=findViewById(R.id.contextTextBox);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                from=fromTextBox.getText().toString();
+                to=toTextBox.getText().toString();
+                title=titleTextBox.getText().toString();
+                context=contextTextBox.getText().toString();
+
+                Mail mail=new Mail(from,to,title,context);
+                Client client=new Client("localhost",1111);
+
+                boolean result=client.sendMail(mail);
+
+                if(result==true){
+
+                    Toast toast = Toast.makeText(ActivityMailWrite.this,"Success", Toast.LENGTH_LONG);
+                    toast.show();
+                    Intent myIntent = new Intent(ActivityMailWrite.this, ActivityFacilityList.class);
+                    ActivityMailWrite.this.startActivity(myIntent);
+                }
+                else{
+                    Toast toast = Toast.makeText(ActivityMailWrite.this,"Failed.", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/ncku/firesimplemail/Mail.java
+++ app/src/main/java/ncku/firesimplemail/Mail.java
@@ -2,14 +2,33 @@ package ncku.firesimplemail;
 
 import java.io.Serializable;
 
-public class Mail extends MailHead implements Serializable{
+public class Mail implements Serializable{
 
     private String body;
+    private String from,to,title;
 
-    public Mail(Id id,String from,String to,String title,String body){
-        super(id,from,to,title);
+    public Mail(String from,String to,String title,String body){
+        this.from=from;
+        this.to=to;
+        this.title=title;
         this.body=body;
     }
+    public Mail(String from,String to,String title){
+        this.from=from;
+        this.to=to;
+        this.title=title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public String getFrom(){
+        return from;
+    }
+    public String getTo(){
+        return to;
+    }
+
     public String getBody(){
         return "Hello";
     }
